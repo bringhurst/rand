b2a431822f7b8a495b0707dd0bd45dbbb87f573a
Jon Bringhurst <jonb@lanl.gov>
2011-06-16 11:54:40 -0600

    Added warning for password length. Aligned random stream to base64.

--- rand.php
+++ rand.php
@@ -12,15 +12,16 @@
     <h2>cryptographic quality random password generator</h2>
     <h3>generated with openssl using the default linux non-hw prng<br />
     each 14 character password has approx 84 bits of entropy (NIST recommends 80)</h3>
+    <p>WARNING: Your password may NOT be secure if you do not use all 14 characters (symbol group size is only 64).</p>
     <?php
       echo "<b>available pool = ";
       $entropy_avail = system('cat /proc/sys/kernel/random/entropy_avail');
       echo "</b>";
       if($entropy_avail < 150) {
-        echo "<p><font color=red>WARNING: Not enough entropy is available. Please try again later.</font></p>";
+        echo "<p><font color=red>WARNING: Not enough entropy is available. Please try again in a few seconds.</font></p>";
       } else {
         echo "<pre><p>START OF PASSWORD LIST</p>";
-        system("/usr/bin/openssl rand -base64 128|/bin/cut -c0-14");
+        system("/usr/bin/openssl rand -base64 120|/bin/cut -c0-14");
         echo "<p>END OF PASSWORD LIST</p></pre>";
       }
     ?>
